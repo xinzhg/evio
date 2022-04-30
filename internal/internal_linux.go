d0e421ac305c884574a0f090d0c0305c1b40789e
Xin Zhang <xinzhang@Mikes-iMac.local>
2022-04-30 15:25:20 -0700

    Use -1 in epoll wait

--- internal/internal_linux.go
+++ internal/internal_linux.go
@@ -54,7 +54,7 @@ func (p *Poll) Trigger(note interface{}) error {
 func (p *Poll) Wait(iter func(fd int, note interface{}) error) error {
 	events := make([]syscall.EpollEvent, 64)
 	for {
-		n, err := syscall.EpollWait(p.fd, events, 100)
+		n, err := syscall.EpollWait(p.fd, events, -1)
 		if err != nil && err != syscall.EINTR {
 			return err
 		}
